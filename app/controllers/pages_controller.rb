df877f5d167e2a2ba70c6762016f057b94b72e59
Your Name <arssonist@yahoo.com>
2016-08-24 15:14:27 -0400

    about page working for routing intro

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
     # render :about
   end
 
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   get '/' => 'pages#welcome'
   #root home page
   get '/welcome' => 'pages#welcome'
+
+  get '/about' => 'pages#about'
+
   #with /welocome after home url
 
 
-  
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
