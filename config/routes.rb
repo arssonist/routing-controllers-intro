a92440471ef5f9c9c7c206da58fce11043233754
Your Name <arssonist@yahoo.com>
2016-08-24 15:00:17 -0400

    welcome and about routes in railsrouting-controllers-intro

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  get '/' => 'pages#welcome'
+  #root home page
+  get '/welcome' => 'pages#welcome'
+  #with /welocome after home url
+
+
+  
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
